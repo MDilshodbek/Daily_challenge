0d3652153531c5935a1cfe022b51ab5d5b32d8cc
MDilshodbek <mdbek55@gmail.com>
2026-01-31 21:42:08 +0900

    feat: finding longest palindrome

--- index.js
+++ index.js
@@ -22,15 +22,64 @@
 // console.log(lengthOfLongestSubstring("bbbbb"));
 // console.log(lengthOfLongestSubstring("pwwkew"));
 
-const findMedianSortedArrays = (nums1, nums2) => {
-  let merged = [...nums1, ...nums2].sort((a, b) => a - b);
-  let mid = Math.floor(merged.length / 2);
+// const findMedianSortedArrays = (nums1, nums2) => {
+//   let merged = [...nums1, ...nums2].sort((a, b) => a - b);
+//   let mid = Math.floor(merged.length / 2);
 
-  if (merged.length % 2 === 0) {
-    return (merged[mid - 1] + merged[mid]) / 2;
+//   if (merged.length % 2 === 0) {
+//     return (merged[mid - 1] + merged[mid]) / 2;
+//   }
+//   return merged[mid];
+// };
+
+// const result = findMedianSortedArrays([1, 2], [3, 4, 5]);
+// console.log("result:", result);
+
+// var longestPalindrome = function(s) {
+//      let maxLength = 0;
+
+//   for (let i = 0; i < s.length; i++) {
+//     let current = "";
+
+//     for (let j = i; j < s.length; j++) {
+//       if (current.includes(s[j])) {
+//         break;
+//       }
+
+//       current += s[j];
+
+//       maxLength = Math.max(maxLength, current.length);
+//     }
+//   }
+
+//   return maxLength;
+// };
+
+const longestPalindrome = (s) => {
+  let startIndex = 0;
+  let maxLength = 1;
+
+  function expand(left, right) {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      left--;
+      right++;
+    }
+
+    const length = right - left - 1;
+
+    if (length > maxLength) {
+      maxLength = length;
+      startIndex = left + 1;
+    }
   }
-  return merged[mid];
-};
 
-const result = findMedianSortedArrays([1, 2], [3, 4, 5]);
+  for (let i = 0; i < s.length; i++) {
+    expand(i, i);
+    expand(i, i + 1);
+  }
+
+  return s.substring(startIndex, startIndex + maxLength);
+}
+
+const result = longestPalindrome("habibi");
 console.log("result:", result);
